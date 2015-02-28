6e2c1c0c0b8066ec710f967eb015e35e52330f05
Nuno Teixeira <nuno.miguel.t@gmail.com>
2015-02-28 21:45:03 +0000

    Update new_test.js

--- new_test.js
+++ new_test.js
@@ -1,7 +1,27 @@
 function updateManifestVersion(filename, version) {
    readFile("")
+   if(a == b) {
+      return;
+   }
 }
 function readFile(filename) {
 
 }
 
+function main(meaning) {
+  var day = true;
+
+  if (meaning === 42) {
+    while (day) {
+      shuffle();
+
+      if (tired) { // JSHint: Blocks are nested too deeply (3).
+          sleep();
+      }
+      else {
+         if(true) return false;
+         console.log(123);
+      }
+    }
+  }
+}
